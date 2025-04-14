a7e1904b344a85ec308fade94ab31922ceaaca15
UQvillain <158751010+UQvillain@users.noreply.github.com>
2025-04-14 11:47:45 +0700

    Delete Design-Medpart directory

--- Design-Medpart/src/components/MediaPartnerComponents/MediaCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MediaCard({ image, title }) {
-    return (
-      <div className="media-card-medpart">
-        <img src={image} alt={title} className="media-image" />
-        <div className="media-title">{title}</div>
-      </div>
-    );
-  }
-  
